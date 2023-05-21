6b2667856ed0491e2bcb15345ad539f1431b707e
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-05-21 17:57:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
-
+	var boldWords = document.querySelectorAll('strong');
+            for (var i = 0; i < boldWords.length; i++) {
+                boldWords[i].classList.add('highlight');
+            }
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	var boldWords = document.querySelectorAll('strong');
+            for (var i = 0; i < boldWords.length; i++) {
+                boldWords[i].classList.remove('highlight');
+            }
 }
